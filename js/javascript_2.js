21ad23bc13eb174c71b61712dfa2dd7d5740120d
tgastudillo <tgastudillo@uc.cl>
2021-10-04 11:48:05 -0300

    cambios
    incorporación de main_1 a main_2
    falta insertar datos

---
+++ js/javascript_2.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+
+  var regiones = document.getElementById('mapa').childNodes;
+  var btn = document.getElementById('button1');
+  var mapa_svg = document.getElementById('mapa');
+  var grafico1 = document.getElementById('grafico1');
+  var grafico2 = document.getElementById('grafico2');
+  btn.onclick = function (event) {
+    if (mapa_svg.classList.contains('animar-mapa')) {
+      mapa_svg.classList.remove('animar-mapa');
+      grafico1.classList.remove('animar-grafico1');
+      grafico2.classList.remove('animar-grafico2');
+      mapa_svg.classList.add('animar-mapa-vuelta');
+      grafico1.classList.add('animar-grafico1-vuelta');
+      grafico2.classList.add('animar-grafico2-vuelta');
+
+    }
+    else if (mapa_svg.classList.contains('animar-mapa-vuelta')) {
+      mapa_svg.classList.remove('animar-mapa-vuelta');
+      grafico1.classList.remove('animar-grafico1-vuelta');
+      grafico2.classList.remove('animar-grafico2-vuelta');
+      mapa_svg.classList.add('animar-mapa');
+      grafico1.classList.add('animar-grafico1');
+      grafico2.classList.add('animar-grafico2');
+    }
+    else {
+      mapa_svg.classList.add('animar-mapa');
+      grafico1.classList.add('animar-grafico1');
+      grafico2.classList.add('animar-grafico2');
+    }
+    /*    setTimeout(function(){
+          mapa_svg.classList.remove('animar-mapa');
+        }, 2000);*/
+  }
+  regiones.forEach(item => {
+    item.onclick = function (event) {
+      console.log(item.getAttribute('title'))
+      regiones.forEach(i => {
+        if (i.nodeName == 'path') {
+          i.style.fill = ""
+        }
+      })
+      item.style.fill = "green";
+    }
+  });
+
+}
