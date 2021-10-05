21021ba9a0b80cee0218b53b038982db2010bcf9
tgastudillo <tgastudillo@uc.cl>
2021-10-04 22:07:03 -0300

    agregado datos regiones

--- js/javascript_2.js
+++ js/javascript_2.js
@@ -41,14 +41,18 @@ window.onload = function () {
           mapa_svg.classList.remove('animar-mapa');
         }, 2000);*/
   }
-  var name = "data/graficos/Índiceprecipitaciónanual_2021_"
+  var name1 = "data/graficos/Índiceprecipitaciónanual_2021_"
+  var name2 = "data/graficos/Índice_sequía_estandarizada_"
   regiones.forEach(item => {
     item.onclick = function (event) {
       console.log(item.getAttribute('title'))
       limpiar_regiones(regiones)
       item.style.fill = "green";
       grafico1.children[0].innerHTML = item.getAttribute('title')
-      grafico1.children[1].setAttribute('src', name+item.getAttribute('title')+'.png')
+      grafico1.children[1].setAttribute('src', name1+item.getAttribute('title')+'.png')
+      grafico2.children[0].innerHTML = "SEQUÍA ESTANDARIZADA " + item.getAttribute('title')
+      grafico2.children[1].setAttribute('src', name2+item.getAttribute('title')+'.png')
+      
     }
   });
 
