64d1f9f3a567a83370435f51aeb0e490da9143ad
Juaconsio <jconchn@uc.cl>
2021-10-11 20:23:15 -0300

    data indices

--- data/data.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import os.path as path
-
-name = input()
-data = pd.read_csv(path.join('data','data', f"{name}.csv"))
-plot = plt.plot(data['Ano'], data['Valor (mm)'])
-plt.savefig(f"data\graficos\{name}.png")
